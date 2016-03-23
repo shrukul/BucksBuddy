1b5145b3fd8473926fe9785daef873a44ff3a7a0
Shrukul <shrukul99@gmail.com>
2016-03-23 12:46:18 +0530

    Update About Activity

--- app/src/main/java/in/bucksbuddy/bucksbuddy/About.java
+++ app/src/main/java/in/bucksbuddy/bucksbuddy/About.java
@@ -43,13 +43,6 @@ import java.util.List;
  */
 public class About extends Fragment {
 
-    private List<Person> foundTeam;
-    private List<Person> techTeam;
-    private List<Person> pubTeam;
-    private RecyclerView rvabout1, rvabout2, rvabout3;
-    MyLinearLayoutManager llm1, llm2, llm3;
-
-
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View v = inflater.inflate(R.layout.about, container, false);
@@ -59,71 +52,5 @@ public class About extends Fragment {
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-
-/*        llm1 = new MyLinearLayoutManager(getActivity());
-        rvabout1 = (RecyclerView) getView().findViewById(R.id.rvabout1);
-        rvabout1.setLayoutManager(llm1);
-        rvabout1.setHasFixedSize(true);
-
-        llm2 = new MyLinearLayoutManager(getActivity());
-        rvabout2 = (RecyclerView) getView().findViewById(R.id.rvabout2);
-        rvabout2.setLayoutManager(llm2);
-        rvabout2.setHasFixedSize(true);
-
-        llm3 = new MyLinearLayoutManager(getActivity());
-        rvabout3 = (RecyclerView) getView().findViewById(R.id.rvabout3);
-        rvabout3.setLayoutManager(llm3);
-        rvabout3.setHasFixedSize(true);
-
-        AsyncDataClass asyncRequestObject = new AsyncDataClass();
-        asyncRequestObject.execute();*/
-
     }
-
-/*    private void initializeData() {
-        foundTeam = new ArrayList<>();
-        foundTeam.add(new Person("Srinath Chamarthi", "NITK Surathkal", R.drawable.srinath, ""));
-        foundTeam.add(new Person("Pavan Vellampalli", "NITK Surathkal", R.drawable.pavan, ""));
-
-        techTeam = new ArrayList<>();
-        techTeam.add(new Person("Shrukul Habib", "NITK Surathkal", R.drawable.shrukul, ""));
-
-        pubTeam = new ArrayList<>();
-        pubTeam.add(new Person("Tejaswi Ravu", "NITK Surathkal", R.drawable.tejaswi, ""));
-        pubTeam.add(new Person("S Harshavardhan Reddy", "NITK Surathkal", R.drawable.harsh, ""));
-    }
-
-    private void initializeAdapter() {
-        RVAdapterAbout adapter = new RVAdapterAbout(foundTeam);
-        rvabout1.setAdapter(adapter);
-        rvabout1.setHasFixedSize(true);
-        adapter = new RVAdapterAbout(techTeam);
-        rvabout2.setAdapter(adapter);
-        rvabout2.setHasFixedSize(true);
-        adapter = new RVAdapterAbout(pubTeam);
-        rvabout3.setAdapter(adapter);
-        rvabout3.setHasFixedSize(true);
-    }
-
-    private class AsyncDataClass extends AsyncTask<String, Void, String> {
-
-
-        @Override
-
-        protected String doInBackground(String... params) {
-            initializeData();
-            return "";
-        }
-
-        @Override
-
-        protected void onPreExecute() {
-            super.onPreExecute();
-        }
-
-        @Override
-        protected void onPostExecute(String result) {
-            initializeAdapter();
-        }
-    }*/
 }
